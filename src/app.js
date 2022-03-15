5f6c6413dec01457a30b9a8a1c9adf2dd7c278f2
Caio Torres <caiocftorres@gmail.com>
2022-03-15 19:17:33 -0300

    feat: create health controller

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
+import 'dotenv/config';
 import app from './express.js';
 import { log } from './commons/index.js';
+import HealthCheck from './services/HealthCheck.js';
 
 const enableExpress = () => {
-  app.listen(app.settings, () => {
+  const server = app.listen(app.settings, () => {
     log.debug(`Express is listening on port ${app.settings.port}`);
   });
+
+  new HealthCheck().execute().catch(() => server.close());
 };
 
 enableExpress();
---
+++ src/services/HealthCheck.js
@@ -0,0 +1,17 @@
+import { sequelize } from '../commons/dbConnection.js';
+import { log } from '../commons/index.js';
+import HttpError from '../errors/HttpError.js';
+
+export default class HealthCheck {
+  async execute() {
+
+    try {
+      await sequelize.authenticate();
+      return 'Sequelize connected sucessfully';
+
+    } catch (error) {
+      log.error(error);
+      throw new HttpError('Sequelize failed to connect', 500);
+    }
+  }
+}
